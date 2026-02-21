f2c1b079dc74b4543d6272486e400a705478bcbb
The Dev Alex <123931069+iamspeedyalex@users.noreply.github.com>
2026-02-20 23:35:23 -0600

    Feb 20: Add 7kyu solution: you're a square

---
+++ codewars/7kyu/2026-Feb/20-youre-a-square.js
@@ -0,0 +1,6 @@
+var isSquare = function(n){
+  let sqrtResult = Math.sqrt(n);
+  return Number.isInteger(sqrtResult);
+}
+
+/* Number.isInteger is a built-in method that checks if a value is a whole number. */
