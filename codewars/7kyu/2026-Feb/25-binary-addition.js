f5bc64c12911e0a78c231be0be05e87aa4928309
The Dev Alex <123931069+iamspeedyalex@users.noreply.github.com>
2026-02-25 05:24:18 -0600

    Feb 25: Add 7kyu kata solution: binary addition

---
+++ codewars/7kyu/2026-Feb/25-binary-addition.js
@@ -0,0 +1,11 @@
+function addBinary(a,b) {
+  let sum = a + b;
+  return sum.toString(2)
+  
+  /*  - add the two numbers together
+      - convert the sum to binary (base 2)
+      - return as a string */
+
+}
+
+/* .toString(2) converts a number to binary string. This is interesting. I didn't know this could be done. */
