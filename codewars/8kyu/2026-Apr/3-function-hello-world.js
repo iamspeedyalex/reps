4edeb11cd831b25ded8cff9dfee0408c1d31ed44
The Dev Alex <123931069+iamspeedyalex@users.noreply.github.com>
2026-04-03 04:57:12 -0500

    Apr 3: Add 8kyu kata solution: function 1 hello world

---
+++ codewars/8kyu/2026-Apr/3-function-hello-world.js
@@ -0,0 +1,17 @@
+function greet() {
+    return "hello world!";
+  }
+  
+  function greet() {
+    const h = "h";
+    const e = "e";
+    const l = "l";
+    const o = "o";
+    const w = "w";
+    const r = "r";
+    const d = "d";
+    
+    return h + e + l + l + o + " " + w + o + r + l + d +"!";
+  }
+
+  console.log(greet());
