e13013c2f334b4584a555e2bc11ab1a5f115fd70
The Dev Alex <123931069+iamspeedyalex@users.noreply.github.com>
2026-04-20 00:33:54 -0500

    added best practices notes from solutions by others

--- codewars/8kyu/2026-Apr/19-squaren-sum.js
+++ codewars/8kyu/2026-Apr/19-squaren-sum.js
@@ -8,3 +8,19 @@ function squareSum(numbers) {
 
     return sum;
 }
+
+/* Best practices note #1: 
+function squareSum(numbers){
+  return numbers.reduce(function(sum, n){
+    return (n*n) + sum;
+  }, 0)
+}
+  starting to learn about callback functions cuz of this. was confused at first, since saw function within a function. with the 0 being the starting value for sum
+*/
+
+/* Best practices note #2: 
+function squareSum(numbers){
+  return numbers.reduce((sum,num) => sum + (num * num), 0);
+}
+  a shorter way to write it
+*/
