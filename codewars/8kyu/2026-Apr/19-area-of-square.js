c182e0ccbf31cb1a6d78a8f60d6292712a9ff3a5
The Dev Alex <123931069+iamspeedyalex@users.noreply.github.com>
2026-04-20 00:12:12 -0500

    area of square best practices added as notes

--- codewars/8kyu/2026-Apr/19-area-of-square.js
+++ codewars/8kyu/2026-Apr/19-area-of-square.js
@@ -6,3 +6,15 @@ function squareArea(A) {
     let side = (2 * A) / Math.PI;
     return side * side;
 }
+
+/* Best practices note #1: 
+const squareArea = A => (2 * A / Math.PI) ** 2
+a side of the square to the power of 2
+*/
+
+/* Best practices note #2: 
+function squareArea(A){
+  return Math.pow(2 * A/Math.PI, 2)
+} 
+  Math.pow(base, exponent)
+*/
