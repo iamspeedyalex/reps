a1b44ed8159f52804c2e87b4b6654e3a21aac65b
The Dev Alex <123931069+iamspeedyalex@users.noreply.github.com>
2026-05-17 00:22:49 -0500

    Add 8kyu kata solution: squaring an argument

---
+++ codewars/8kyu/2026-May/16-squaring-an-argument.js
@@ -0,0 +1,7 @@
+// Write the "square"-function here
+
+function square(argument) {
+    return argument * argument;
+  }
+
+/* Could use ** instead for argument ** 2 since ** is the "power of" operator */
