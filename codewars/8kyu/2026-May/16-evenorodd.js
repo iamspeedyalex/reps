b513709c448ab65cca0261d745f4ef0d537938c1
The Dev Alex <123931069+iamspeedyalex@users.noreply.github.com>
2026-05-17 00:19:58 -0500

    Add 8kyu kata solution: even or odd

---
+++ codewars/8kyu/2026-May/16-evenorodd.js
@@ -0,0 +1,15 @@
+function evenOrOdd(number) {
+    if (number % 2 === 0){
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+/* single line solution by another person
+
+function evenOrOdd(number) {
+    return number % 2 === 0 ? 'Even' : 'Odd';
+  } */
+
+/* ? means if yes; : means otherwise */
