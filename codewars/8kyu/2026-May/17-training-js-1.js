4b5094b492b77eaafbb722afc487d0107596fc58
The Dev Alex <123931069+iamspeedyalex@users.noreply.github.com>
2026-05-17 23:43:54 -0500

    Add 8kyu kata solution: training js 1

---
+++ codewars/8kyu/2026-May/17-training-js-1.js
@@ -0,0 +1,13 @@
+function printWordToScreen() {
+    var somewords = "This is an example."
+    console.log(somewords);
+}
+
+printWordToScreen()
+
+function helloWorld() {
+    let str = "Hello World!"
+    console.log(str);
+}
+
+helloWorld()
